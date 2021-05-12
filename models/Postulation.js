ff2f8e8dc98a20642e303df8879134dedf9ee139
jperlaza <perlazajohao@gmail.com>
2021-05-11 20:40:20 -0500

    knownledge_area added

--- models/Postulation.js
+++ models/Postulation.js
@@ -5,16 +5,16 @@ const Postulation = new Schema({
   title_project: { type: String },
   summary_project: { type: String },
   summary_pdf: { type: String },
-  knowledge_area: {
-    type: String,
-    enum: [
-      "Artes y Humanidades",
-      "Ciencias",
-      "Ciencias de la Salud",
-      "Ciencias Sociales y Jurídicas",
-      "Ingeniería y Arquitectura",
-    ],
-  },
+  // knowledge_area: {
+  //   type: String,
+  //   enum: [
+  //     "Artes y Humanidades",
+  //     "Ciencias",
+  //     "Ciencias de la Salud",
+  //     "Ciencias Sociales y Jurídicas",
+  //     "Ingeniería y Arquitectura",
+  //   ],
+  // },
   files: { type: String },
   presentation_date: { type: Date },
   person_id: { type: String },
