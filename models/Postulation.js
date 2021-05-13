c6d059757d82085aa10a55f566a94b463bedc4f1
vimhash <perlazajohao@gmail.com>
2021-05-12 21:44:56 -0500

    knowledge area postulation

--- models/Postulation.js
+++ models/Postulation.js
@@ -5,16 +5,7 @@ const Postulation = new Schema({
   title_project: { type: String },
   summary_project: { type: String },
   summary_pdf: { type: String },
-  // knowledge_area: {
-  //   type: String,
-  //   enum: [
-  //     "Artes y Humanidades",
-  //     "Ciencias",
-  //     "Ciencias de la Salud",
-  //     "Ciencias Sociales y Jurídicas",
-  //     "Ingeniería y Arquitectura",
-  //   ],
-  // },
+  knowledge_area: { type: String },
   files: { type: String },
   presentation_date: { type: Date },
   person_id: { type: String },
