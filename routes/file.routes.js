1a5cb8ee8ea72124123d97414fafa9fc37a70710
JohaoDev <perlazajohao@gmail.com>
2021-08-19 22:02:02 -0500

    changes

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -5,16 +5,19 @@ const express = require("express"),
 let api = express.Router();
 
 api.get("/file/:directory/:urlFile", fileController.showFile);
+
 api.post(
   "/file/upload/:directory",
   [authenticate.tokenAuth],
   fileController.uploadFile
 );
+
 api.put(
   "/file/:directory/:urlFile",
   [authenticate.tokenAuth],
   fileController.modifyFile
 );
+
 api.delete(
   "/file/:directory/:urlFile",
   [authenticate.tokenAuth],
